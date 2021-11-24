def346166e2c7da6241e3447357b9b6eedf092d4
Zack0612 <a22400530@gmail.com>
2021-11-24 23:28:06 +0800

    11

--- js/script.js
+++ js/script.js
@@ -1,28 +1,5 @@
 $(function () {
 
-
-
-    // media   ==================================================
-    if ($(window).width() < 767) {
-        // JavaScript here
-        // 當視窗寬度小於767px時執行
-        $('.go-top').click(function () {
-            // $('html,body').css('overflow', '');
-            // $('#section1,#section2,#section3,#section4,#section5,#section6,.footer_').fadeIn();
-            $('.pages').removeClass('scaleB');
-
-
-        });
-
-    } else {
-        // JavaScript here
-        // 當視窗寬度不小於767px時執行
-        $('.go-top').click(function () {
-            $('.pages').removeClass('scaleB');
-        });
-
-    }
-
     //common pages
     // fullpage =============================================
     var myFullpage = new fullpage('#wrapper', {
