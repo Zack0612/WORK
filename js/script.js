1e95fd1afd0dfbc76bd7962e3863c0e4db016815
Zack0612 <a22400530@gmail.com>
2021-11-26 12:38:33 +0800

    11

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ $(function () {
     //common pages
     // fullpage =============================================
     var myFullpage = new fullpage('#wrapper', {
-
-
         anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage'],
         navigation: true,
         navigationPosition: 'right',
@@ -35,51 +33,65 @@ $(function () {
         }
     }
 
-    $(document).on('click', '#fp-nav', function(){
+    let startY;
+    document.addEventListener("touchstart", function (event) {
+        event.preventDefault();
+        startY = event.changedTouches[0].pageY;
+    }, {passive: false})
+
+    let moveEndY;
+    document.addEventListener("touchmove", function (event) {
+        event.preventDefault();
+        moveEndY = event.changedTouches[0].pageY;
+        let Y = moveEndY - startY;
+
+        // 手指往上滑,下滑頁面時停止自動輪播
+        if (Y < 0) {
             clearTimeout(timer);
             clearTimeout(distance);
-    });
-
-
-   // $(window).load(
-   //      function(){
-   //      timer = setTimeout(function(){
-   //          fullpage_api.moveTo('secondPage', 1);
-   //      },5000);
-   //      distance = setTimeout(function(){
-   //          fullpage_api.moveTo('3rdPage', 1);
-   //      },15000);
-
-
-   //  window.addEventListener('scroll', function () {
-   //      if (window.scrollY > 0) {
-   //          delete timer; 
-   //          delete distance; 
-   //      }
-   //  })  
-
-   //  })
+        }
+    }, {passive: false})
 
 
     //adding the action to the button
     $(document).on('click', '#moveTo1', function(){
       fullpage_api.moveTo('firstPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
     });
     $(document).on('click', '#moveTo3', function(){
       fullpage_api.moveTo('3rdPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
     });
     $(document).on('click', '#moveTo2', function(){
       fullpage_api.moveTo('secondPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
     });
     $(document).on('click', '#moveTo4', function(){
       fullpage_api.moveTo('4thPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
     });
 
     $(document).on('click', '#gotop', function(){
       fullpage_api.moveTo('firstPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
     });
 
-
+    $(document).on('click', '#logo', function(){
+      fullpage_api.moveTo('4thPage', 1);
+       clearTimeout(timer);
+       clearTimeout(distance);
+    });
+    $(document).on('click', '#scroll', function(){
+      fullpage_api.moveTo('secondPage', 1);
+      clearTimeout(timer);
+      clearTimeout(distance);
+    });
 
  })
+
 
