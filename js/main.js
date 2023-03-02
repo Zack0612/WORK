9f447e5519e1588448730ce9372efa9a32eac8b1
Zack0612 <a22400530@gmail.com>
2023-03-02 10:08:14 +0800

    123

--- js/main.js
+++ js/main.js
@@ -13,19 +13,20 @@ $(document).ready(function(){
     });
 
 
- //TOP_alin_MV pop
- $('.alin_MV_pop').click(function () {
-    $('.alin_MV_test_pop_1').css('display', 'flex').fadeIn(100);
-    $('.alin_MV_inner_pop_1').delay(100).fadeIn(500);
+ //reward_button
+ $('.reward_buttonactive').click(function () {
+    $('.wanzi_popup').css('display', 'flex').fadeIn(100);
+    $('.wanzi_popup').delay(100).fadeIn(500);
 
   });
 
-  $('.alin_MV_btn_close').click(function () {
-    $('.alin_MV_inner_pop_1').fadeOut(100);
-    $('.alin_MV_test_pop_1').fadeOut(100);
-    $(".youtube-video").attr('src', ''); //關閉連結設為空直
+  $('.wanzi_popup_close').click(function () {
+    $('.wanzi_popup').fadeOut(100);
   });
 
+  $('.wanzi_popup_content .ok').click(function () {
+    $('.wanzi_popup').fadeOut(100);
+  });
     
 });
 
